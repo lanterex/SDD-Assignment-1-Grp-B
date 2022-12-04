46939f352ffb1c7c650b9a12ca23ca728a7d6ef7
Dyoneus <s10227735@connect.np.edu.sg>
2022-12-04 22:29:56 +0800

    Implement Game Menu

--- SDD Assignment 2.py
+++ SDD Assignment 2.py
@@ -1,1 +1,24 @@
-print('Hello World!')
+import random # To be able to get a random number
+import sys    # For me to be able to close the game   
+
+print('Welcome, Mayor of Ngee Ann City!')
+print('----------------------------')
+
+def first_screen():
+    global buildingList,turnnumber,map_grid
+    print('1. Start new game')
+    print('2. Load saved game')
+    print('\n0. Exit')
+    sogchoice = int(input('Your choice? '))
+    if sogchoice == 1:                
+        return 1
+    if sogchoice == 2:
+        print('\n Loading... \n')
+        return 1
+    if sogchoice == 0:
+        print('Thanks for playing!')
+        sys.exit()
+        
+while True:
+    option = first_screen()
+    exit_main_screen = False
