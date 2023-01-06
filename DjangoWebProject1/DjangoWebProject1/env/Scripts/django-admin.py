0ade8cc5a84574e81e663b3afdb2bc724e45ec86
ArkTorch3rd <tjwgareth@gmail.com>
2023-01-06 22:17:03 +0800

    cleaning the directory

--- DjangoWebProject1/DjangoWebProject1/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\CodeFiles\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
